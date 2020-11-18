02673f42f2163743ab1212828f9555a2e8669c1f
vwyborski16osu <72372128+vwyborski16osu@users.noreply.github.com>
2020-11-17 22:36:40 -0800

    Vince Development Work
    Modified databaseConnection.js to work for simple example query.
    Added basic_profile_Modfy.html to add in buttons and search bars for adding and removing skills.
    Installed NPM and saved packages to package-lock.json
    Added searchScript.js to add button functionality.
    Changed to handlebars layout. Changed the layout.html to layout.handlebars to test functionality, and added folder structure copied from CS290 to enable functionality.

--- Webpage/databaseConnection.js
+++ Webpage/databaseConnection.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var mysql = require("mysql");
+var bodyParser = require('body-parser');
 
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', '8352');
+app.set('port', 8352);
+var port = 8352;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static("public"));
@@ -23,14 +25,18 @@ var pool = mysql.createPool({
 // and last name.
 app.get('/', function(req, res, next)
 {
-    var sqlStatement = 'SELECT * FROM Users INNER JOIN ExpertSubjects INNER JOIN ExpertLinks INNER JOIN ExpertClasses WHERE Users.lastName = ? AND Users.firstName = ? AND Users.user_id = ExpertSubjects.user_id AND Users.user_id = ExpertLinks.user_id AND Users.user_id = ExpertClasses.user_id', [req.query.lastname], [req.query.firstname];
+    console.log("boobs");
+    /*var sqlStatement = 'SELECT * FROM Users INNER JOIN ExpertSubjects INNER JOIN ExpertLinks INNER JOIN ExpertClasses WHERE Users.lastName = ? AND Users.firstName = ? AND Users.user_id = ExpertSubjects.user_id AND Users.user_id = ExpertLinks.user_id AND Users.user_id = ExpertClasses.user_id', [req.query.lastname], [req.query.firstname];
     pool.query(sqlStatement, function(err, result, fields)
     {
         var userInfo = JSON.stringify(result);
         res.send(userInfo);
     });
+    */
+    res.render('layout');
 });
 
+/*
 // This should create a new user in the user database
 app.get('/add', function(req, res, next)
 {
@@ -42,4 +48,35 @@ app.get('/add', function(req, res, next)
         res.send(sendData);
         });
         
+});
+
+app.get('/addCourse', function(req, res, next)
+{
+    var sqlStatement = 'INSERT INTO ExpertSubjects (user_id, subject_id) VALUES (?, ?)', [req.query.user_id, req.query.subject_id];
+    
+    pool.query(sqlStatement, function(err, rows, fields)
+        {
+        var sendData = JSON.stringify(rows);
+        res.send(sendData);
+        });
+        
+});
+*/
+app.get('/searchSkill', function(req, res, next)
+{
+    
+    var sqlStatement = "SELECT * FROM `Subjects` WHERE `description` = 'Python'";
+    pool.query(sqlStatement, function(err, result, fields)
+    {
+        var userInfo = JSON.stringify(result);
+        console.log(userInfo);
+        res.send(userInfo);
+    });
+    
+
+});
+
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
