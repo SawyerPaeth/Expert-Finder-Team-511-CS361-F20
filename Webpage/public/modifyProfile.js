eb1104b1890b8d5f0dc14ccf0e2d03c47805fe22
Will Levin <levinw@oregonstate.edu>
2020-11-22 08:56:08 -0700

    Added comment to outline db stuff

--- Webpage/public/modifyProfile.js
+++ Webpage/public/modifyProfile.js
@@ -5,6 +5,27 @@ function bindButtons(){
     var req = new XMLHttpRequest();
     var payload = {userID:2, skill:3};
     //payload.skill = document.getElementById('searchSkillInput').value;
+    
+    // Insert into the skills table sql statement 
+    // Ok inserting a skill against a user takes three steps:
+    // 1. Insert the subject here - replace Subjects with classes.  Once it does
+    //    the database will autopopulate the key (unique identifier) with an incremented 
+    //    value. The description is something like "Python" or "C++" 
+    //sqlStatement = "INSERT INTO Subjects (description) VALUES (?, ?, ?, ?)", [<user input>]];
+    // 2. Do a select statement to pull the identifier from the database.  
+    //  var sqlStatement = "SELECT * FROM `Subjects` WHERE description = <user input>;
+    // pool.query(sqlStatement, function(err, result, fields)
+    // {
+    //    var userInfo = JSON.stringify(result);
+    //    console.log(userInfo);
+    //    res.send(userInfo);
+    //});
+    // 3. Use the pulled statement to insert the skill against the user.
+    // var sqlStatement = "INSERT INTO ExpertSubjects VALUES (?, ?)", [<id of user>, <step 2 value>];
+    // 
+    // Alternatively you can combine the sql statements in 2 and 3, but I would recommend sticking 
+    // with this simplistic view to minimize complexity and allow for an easier debugging experience
+    // You should have the user id from the profile pulled already
 
     req.open('POST', '/basicProfileModify/addSkill', true);
     req.setRequestHeader('Content-Type', 'application/json');
