ecf11f6f1a845534d08864bd51232142f6884b57
vwyborski16osu <72372128+vwyborski16osu@users.noreply.github.com>
2020-11-22 19:01:11 -0800

    UI Cleanup
    Combined search functions into 1 search page. Added links on the NavBar to different sections of the website and made the site slightly more functional overall.

--- Webpage/expertFinder.js
+++ Webpage/expertFinder.js
@@ -187,44 +187,24 @@ app.get('/advancedSearch', function(req, res, next)
     res.render('advancedSearch');
 });
 
-app.get('/basicProfile', function(req, res, next){
-    var sqlStatement = "SELECT * FROM Subjects";
-    pool.query(sqlStatement, function(err, result, fields)
-    {
-        var userInfo = JSON.stringify(result);
-        console.log(userInfo);
-        res.render('basicProfile', {
-            result: result
+app.get('/basicProfile', function(req, res, next)
+{
+    /*
+    pool.query(sqlStatement,
+        function (err, rows, fields)
+        {
+            var SendData = JSON.stringify(rows);
+            res.send(sendData);
         });
-    });
-    
+    */
+    res.render('basicProfile');
 });
-
 
 app.get('/basicProfileModify', function(req, res, next)
 {
     res.render('basicProfileModify');
 });
-    // Insert into the skills table sql statement 
-    // Ok inserting a skill against a user takes three steps:
-    // 1. Insert the subject here - replace Subjects with classes.  Once it does
-    //    the database will autopopulate the key (unique identifier) with an incremented 
-    //    value. The description is something like "Python" or "C++" 
-    //sqlStatement = "INSERT INTO Subjects (description) VALUES (?, ?, ?, ?)", [<user input>]];
-    // 2. Do a select statement to pull the identifier from the database.  
-    //  var sqlStatement = "SELECT * FROM `Subjects` WHERE description = <user input>;
-    // pool.query(sqlStatement, function(err, result, fields)
-    // {
-    //    var userInfo = JSON.stringify(result);
-    //    console.log(userInfo);
-    //    res.send(userInfo);
-    //});
-    // 3. Use the pulled statement to insert the skill against the user.
-    // var sqlStatement = "INSERT INTO ExpertSubjects VALUES (?, ?)", [<id of user>, <step 2 value>];
-    // 
-    // Alternatively you can combine the sql statements in 2 and 3, but I would recommend sticking 
-    // with this simplistic view to minimize complexity and allow for an easier debugging experience
-    // You should have the user id from the profile pulled already
+
 app.post('/basicProfileModify/addSkill',function(req,res){
     console.log(req.body)
     pool.query("INSERT INTO Subjects (description) VALUES (?)", req.body.skill, function(err, rows, fields){
